6464351e59ba8df9f48d11b473416054f00b132f
Hannah Bui <hannahbui@Hannahs-MacBook-Air.local>
2025-01-13 22:55:00 -0600

    <Enter your message (without the <brackets>)>

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
+import importlib
+import importlib.machinery
+import importlib.util
 import os
 import random
 import requests
 import tweepy
 
-
-# Pick a random image/video from the 'assets' folder
-def get_random_media():
-    path = 'assets' # 
-    objects = os.listdir(path)
-
-    media = random.choice(objects)
-    return os.path.join(path, media)
+from log import log
+from webp_to_gif import convert_all_webp_to_gif
 
 
-# Authorize Twitter with v1.1 API
-def auth_v1(consumer_key, consumer_secret, access_token, access_token_secret):
+def get_assets(path: str) -> list[str]:
+    loader = importlib.machinery.SourceFileLoader('get_assets', os.path.join(path, 'get_assets.py'))
+    spec = importlib.util.spec_from_loader(loader.name, loader)
+    assets_module = importlib.util.module_from_spec(spec)
+    loader.exec_module(assets_module)
+    return assets_module.get_assets(path)
+
+
+def get_random_assets() -> list[str]:
+    path = 'assets'
+    all_assets = os.listdir(path)
+
+    random.shuffle(all_assets)
+    asset = random.choice(all_assets)
+
+    asset_path = os.path.join(path, asset)
+    if os.path.isfile(asset_path):
+        return [asset_path]
+    
+    if not os.path.isfile(os.path.join(asset_path, 'get_assets.py')):
+        raise FileNotFoundError(f'`get_assets.py` must exist inside `{asset_path}`')
+    
+    return get_assets(asset_path)
+
+
+def auth_v1(consumer_key, consumer_secret, access_token, access_token_secret) -> tweepy.API:
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     return tweepy.API(auth)
 
 
-# Authorize Twitter with v2 API
-def auth_v2(consumer_key, consumer_secret, access_token, access_token_secret):
+def auth_v2(consumer_key, consumer_secret, access_token, access_token_secret) -> tweepy.Client:
     return tweepy.Client(
         consumer_key=consumer_key, consumer_secret=consumer_secret,
         access_token=access_token, access_token_secret=access_token_secret,
@@ -29,9 +49,7 @@ def auth_v2(consumer_key, consumer_secret, access_token, access_token_secret):
     )
 
 
-# Tweet picked image/video
-def tweet(media) -> requests.Response:
-    # In the next for 4 lines we are getting the keys from Step 4
+def tweet(assets: list[str]) -> requests.Response:
     consumer_key = os.environ['CONSUMER_KEY']
     consumer_secret = os.environ['CONSUMER_SECRET']
     access_token = os.environ['ACCESS_TOKEN']
@@ -42,14 +60,21 @@ def tweet(media) -> requests.Response:
     client_v2 = auth_v2(consumer_key, consumer_secret,
                         access_token, access_token_secret)
 
-    media_id = api_v1.media_upload(media).media_id
+    media_ids = [api_v1.media_upload(asset).media_id_string for asset in assets]
 
-    return client_v2.create_tweet(media_ids=[media_id])
+    return client_v2.create_tweet(media_ids=media_ids)
 
 
 def main():
-    medias = get_random_medias()
-    tweet(medias)
+    assets = None
+    try:
+        convert_all_webp_to_gif()
+        assets = get_random_assets()
+        response = tweet(assets)
+    except Exception as e:
+        log(e, assets)
+    else:
+        log(response, assets)
 
 
 if __name__ == '__main__':
